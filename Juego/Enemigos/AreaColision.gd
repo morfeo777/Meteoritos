c4937a7900b65b8cfe2ca943753151fdb474a83e
morfeo777 <oneapp107@gmail.com>
2023-10-17 15:03:41 -0300

    Escudo creo
    -Arreglamo el escudo
    -El Laser todavia no arrregle

---
+++ Juego/Enemigos/AreaColision.gd
@@ -0,0 +1,5 @@
+extends Area2D
+
+func recibir_danio(danio: float):
+	owner.recibir_danio(danio)
+
--- Juego/Proyectiles/Proyectil.gd
+++ Juego/Proyectiles/Proyectil.gd
@@ -10,11 +10,31 @@ func crear(pos: Vector2, dir: float, vel:float, danio_p: int) -> void:
 	position = pos
 	rotation = dir
 	velocidad = Vector2(vel, 0).rotated(dir)
+	danio = danio_p
 
 func _physics_process(delta: float) -> void:
 	position += velocidad * delta
 
-
-
+##Metodos Custom
+func daniar(otro_cuerpo: CollisionObject2D) -> void:
+	print(otro_cuerpo.name)
+	print(otro_cuerpo.owner.name)
+	if otro_cuerpo.has_method("recibir_danio"):
+		otro_cuerpo.recibir_danio(danio)
+	queue_free()
+	
+##Señales Internas
 func _on_VisibilityNotifier2D_screen_exited() -> void:
 	queue_free()
+
+
+func _on_Proyectil_area_entered(area: Area2D) -> void:
+	#if area.has_method("recibir_danio"):
+	#	area.recibir_danio()
+	#queue_free()
+	daniar(area)
+
+
+
+func _on_Proyectil_body_entered(body: Node) -> void:
+	daniar(body)
